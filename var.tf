116c04aee6e7662ab33bb40a881a0cf9ca1ca255
sandhya <sandhyreddyettedi111@gmail.com>
2020-11-09 17:27:26 +0530

    adding terr cloud

---
+++ var.tf
@@ -0,0 +1,8 @@
+variable "access-key"{
+    type = string
+    default = "AKIA4RFZKMQHCYGOFLEJ"
+}
+variable "secret-key"{
+    type = string
+    default = "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"
+}
