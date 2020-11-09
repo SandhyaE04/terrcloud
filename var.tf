7b1add7d50a2480a9c8c4be9b00c653639f85eba
SandhyaE04 <53289644+SandhyaE04@users.noreply.github.com>
2020-11-09 20:21:53 +0530

    Delete var.tf

--- var.tf
+++
@@ -1,8 +0,0 @@
-variable "access-key"{
-    type = string
-    default = "AKIA4RFZKMQHCYGOFLEJ"
-}
-variable "secret-key"{
-    type = string
-    default = "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"
-}
